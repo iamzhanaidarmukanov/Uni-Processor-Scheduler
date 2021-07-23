ebf12526552231609e88c8ad0656a3060afb82e2
Zhanaidar Mukanov <zhanaidar@Zhanaidars-MacBook-Pro.local>
2021-07-23 19:45:27 +0800

    Implement FCFS

--- scheduler.cpp
+++ scheduler.cpp
@@ -133,6 +133,119 @@ int move_process_from(vector<process_t>& q1, vector<process_t>& q2)
 	return 0;
 }
 
+void manage_next_service_fcfs(process_t& cur_process, int& complete_num, int& dispatched_tick,
+	int& cur_tick, vector<process_t>& ready_queue, vector<process_t>& processes_done,
+	vector<process_t>& block_queue_D, vector<process_t>& block_queue_K, vector<process_t>& block_queue_mtx) {
+	bool process_completed = cur_process.proceed_to_next_service();
+	if (process_completed) {		// the whole process is completed
+		complete_num++;
+		cur_process.log_working(dispatched_tick, cur_tick + 1);
+		move_process_from(ready_queue, processes_done);		// remove current process from ready queue
+	}
+	else if (cur_process.cur_service.type == "D") {		// next service is disk I/O, block current process
+		cur_process.log_working(dispatched_tick, cur_tick + 1);
+		move_process_from(ready_queue, block_queue_D);
+	}
+	else if (cur_process.cur_service.type == "K") {		// next service is keyboard input, block current process
+		cur_process.log_working(dispatched_tick, cur_tick + 1);
+		move_process_from(ready_queue, block_queue_K);
+	}
+	else if (cur_process.cur_service.type == "L") {     // next service is mutex lock
+		if (mutex.status_lock == false) {
+			mutex.mutex_lock();
+			manage_next_service_fcfs(cur_process, complete_num, dispatched_tick, cur_tick, ready_queue,
+				processes_done, block_queue_D, block_queue_K, block_queue_mtx); // look for next service
+		}
+		else {
+			cur_process.log_working(dispatched_tick, cur_tick + 1);
+			move_process_from(ready_queue, block_queue_mtx);    // block current process
+		}
+	}
+	else if (cur_process.cur_service.type == "U") {     // next service is mutex unlock
+		if (mutex.status_lock == true) {
+			mutex.mutex_unlock();
+			manage_next_service_fcfs(cur_process, complete_num, dispatched_tick, cur_tick, ready_queue,
+				processes_done, block_queue_D, block_queue_K, block_queue_mtx); // look for next service
+		}
+	}
+}
+
+
+int fcfs(vector<process_t> processes, const char* output_path)
+{
+	vector<process_t> ready_queue;
+	vector<process_t> block_queue_D;
+	vector<process_t> block_queue_K;
+	vector<process_t> processes_done;
+	vector<process_t> block_queue_mtx;
+
+	int complete_num = 0;
+	int dispatched_tick = 0;
+	int cur_process_id = -1, prev_process_id = -1;
+
+	// main loop
+	for (int cur_tick = 0; cur_tick < MAX_LOOP; cur_tick++) {
+
+		// long term scheduler
+		for (int i = 0; i < processes.size(); i++) {
+			if (processes[i].arrival_time == cur_tick) {		// process arrives at current tick
+				ready_queue.push_back(processes[i]);
+			}
+		}
+
+		// disk I/O device scheduling
+		if (!block_queue_D.empty()) {
+			process_t& cur_io_process = block_queue_D.front();	// always provide service to the first process in block queue
+			if (cur_io_process.cur_service_tick >= cur_io_process.cur_service.time_cost) {	// I/O service is completed
+				cur_io_process.proceed_to_next_service();
+				move_process_from(block_queue_D, ready_queue);
+			}
+			cur_io_process.cur_service_tick++;		// increment the num of ticks that have been spent on current service
+		}
+		// keyboard I/O device scheduling
+		if (!block_queue_K.empty()) {
+			process_t& cur_io_process = block_queue_K.front();	// always provide service to the first process in block queue
+			if (cur_io_process.cur_service_tick >= cur_io_process.cur_service.time_cost) {	// I/O service is completed
+				cur_io_process.proceed_to_next_service();
+				move_process_from(block_queue_K, ready_queue);
+			}
+			cur_io_process.cur_service_tick++;		// increment the num of ticks that have been spent on current service
+		}
+		// mutex scheduling
+		if (!block_queue_mtx.empty()) {
+			process_t& cur_mtx_process = block_queue_mtx.front();	// always provide service to the first process in block queue
+			if (cur_mtx_process.cur_service.type == "L") {
+				if (mutex.status_lock == false) {  //lock mutex if unlocked
+					mutex.mutex_lock();
+					cur_mtx_process.proceed_to_next_service();
+					move_process_from(block_queue_mtx, ready_queue);
+				}
+			}
+		}
+		// CPU scheduling
+		if (ready_queue.empty()) {	// no process for scheduling
+			prev_process_id = -1;	// reset the previous dispatched process ID to empty
+		}
+		else {
+			process_t& cur_process = ready_queue.front();	// always dispatch the first process in ready queue
+			cur_process_id = cur_process.process_id;
+			if (cur_process_id != prev_process_id) {		// store the tick when current process is dispatched
+				dispatched_tick = cur_tick;
+			}
+			cur_process.cur_service_tick++;		// increment the num of ticks that have been spent on current service
+			if (cur_process.cur_service_tick >= cur_process.cur_service.time_cost) {	// current service is completed
+				manage_next_service_fcfs(cur_process, complete_num, dispatched_tick, cur_tick, ready_queue,
+					processes_done, block_queue_D, block_queue_K, block_queue_mtx);  // look for next service
+			}
+			prev_process_id = cur_process_id;	// log the previous dispatched process ID
+		}
+		if (complete_num == processes.size()) {	// all process completed
+			break;
+		}
+	}
+	write_file(processes_done, output_path);	// write output
+	return 1;
+}
 
 
 // main Function Start
